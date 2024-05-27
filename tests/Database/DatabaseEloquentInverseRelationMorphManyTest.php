819e1f59a36125926f164701470adede11f612f0
Samuel Levy <sam@determineddevelopment.com>
2024-05-27 16:31:48 +1000

    [11.x] Added inverse() to MorphOne and MorphMany

---
+++ tests/Database/DatabaseEloquentInverseRelationMorphManyTest.php
@@ -0,0 +1,238 @@
+<?php
+
+namespace Illuminate\Tests\Database;
+
+use Illuminate\Database\Capsule\Manager as DB;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Model as Eloquent;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
+use PHPUnit\Framework\TestCase;
+
+class DatabaseEloquentInverseRelationMorphManyTest extends TestCase
+{
+    /**
+     * Setup the database schema.
+     *
+     * @return void
+     */
+    protected function setUp(): void
+    {
+        $db = new DB;
+
+        $db->addConnection([
+            'driver' => 'sqlite',
+            'database' => ':memory:',
+        ]);
+        $db->bootEloquent();
+        $db->setAsGlobal();
+
+        $this->createSchema();
+    }
+
+    protected function createSchema()
+    {
+        $this->schema()->create('test_posts', function ($table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+
+        $this->schema()->create('test_comments', function ($table) {
+            $table->increments('id');
+            $table->morphs('commentable');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Tear down the database schema.
+     *
+     * @return void
+     */
+    protected function tearDown(): void
+    {
+        $this->schema()->drop('test_posts');
+        $this->schema()->drop('test_comments');
+    }
+
+    public function testMorphManyInverseRelationIsProperlySetToParentWhenLazyLoaded()
+    {
+        MorphManyInversePostModel::factory()->count(3)->create();
+        $posts = MorphManyInversePostModel::all();
+
+        foreach ($posts as $post) {
+            $this->assertFalse($post->relationLoaded('comments'));
+            $comments = $post->comments;
+            foreach ($comments as $comment) {
+                $this->assertTrue($comment->relationLoaded('commentable'));
+                $this->assertSame($post, $comment->commentable);
+            }
+        }
+    }
+
+    public function testMorphManyInverseRelationIsProperlySetToParentWhenEagerLoaded()
+    {
+        MorphManyInversePostModel::factory()->count(3)->create();
+        $posts = MorphManyInversePostModel::with('comments')->get();
+
+        foreach ($posts as $post) {
+            $comments = $post->getRelation('comments');
+
+            foreach ($comments as $comment) {
+                $this->assertTrue($comment->relationLoaded('commentable'));
+                $this->assertSame($post, $comment->commentable);
+            }
+        }
+    }
+
+    public function testMorphManyInverseRelationIsProperlySetToParentWhenMakingMany()
+    {
+        $post = MorphManyInversePostModel::create();
+
+        $comments = $post->comments()->makeMany(array_fill(0, 3, []));
+
+        foreach ($comments as $comment) {
+            $this->assertTrue($comment->relationLoaded('commentable'));
+            $this->assertSame($post, $comment->commentable);
+        }
+    }
+
+    public function testMorphManyInverseRelationIsProperlySetToParentWhenCreatingMany()
+    {
+        $post = MorphManyInversePostModel::create();
+
+        $comments = $post->comments()->createMany(array_fill(0, 3, []));
+
+        foreach ($comments as $comment) {
+            $this->assertTrue($comment->relationLoaded('commentable'));
+            $this->assertSame($post, $comment->commentable);
+        }
+    }
+
+    public function testMorphManyInverseRelationIsProperlySetToParentWhenCreatingManyQuietly()
+    {
+        $post = MorphManyInversePostModel::create();
+
+        $comments = $post->comments()->createManyQuietly(array_fill(0, 3, []));
+
+        foreach ($comments as $comment) {
+            $this->assertTrue($comment->relationLoaded('commentable'));
+            $this->assertSame($post, $comment->commentable);
+        }
+    }
+
+    public function testMorphManyInverseRelationIsProperlySetToParentWhenSavingMany()
+    {
+        $post = MorphManyInversePostModel::create();
+        $comments = array_fill(0, 3, new MorphManyInverseCommentModel);
+
+        $post->comments()->saveMany($comments);
+
+        foreach ($comments as $comment) {
+            $this->assertTrue($comment->relationLoaded('commentable'));
+            $this->assertSame($post, $comment->commentable);
+        }
+    }
+
+    public function testMorphManyInverseRelationIsProperlySetToParentWhenUpdatingMany()
+    {
+        $post = MorphManyInversePostModel::create();
+        $comments = MorphManyInverseCommentModel::factory()->count(3)->create();
+
+        foreach ($comments as $comment) {
+            $this->assertTrue($post->isNot($comment->commentable));
+        }
+
+        $post->comments()->saveMany($comments);
+
+        foreach ($comments as $comment) {
+            $this->assertSame($post, $comment->commentable);
+        }
+    }
+
+    /**
+     * Helpers...
+     */
+
+    /**
+     * Get a database connection instance.
+     *
+     * @return \Illuminate\Database\Connection
+     */
+    protected function connection($connection = 'default')
+    {
+        return Eloquent::getConnectionResolver()->connection($connection);
+    }
+
+    /**
+     * Get a schema builder instance.
+     *
+     * @return \Illuminate\Database\Schema\Builder
+     */
+    protected function schema($connection = 'default')
+    {
+        return $this->connection($connection)->getSchemaBuilder();
+    }
+}
+
+class MorphManyInversePostModel extends Model
+{
+    use HasFactory;
+
+    protected $table = 'test_posts';
+    protected $fillable = ['id'];
+
+    protected static function newFactory()
+    {
+        return new MorphManyInversePostModelFactory();
+    }
+
+    public function comments(): MorphMany
+    {
+        return $this->MorphMany(MorphManyInverseCommentModel::class, 'commentable')->inverse('commentable');
+    }
+}
+
+class MorphManyInversePostModelFactory extends Factory
+{
+    protected $model = MorphManyInversePostModel::class;
+
+    public function definition()
+    {
+        $this->has(MorphManyInverseCommentModel::factory()->count(3));
+
+        return [];
+    }
+}
+
+class MorphManyInverseCommentModel extends Model
+{
+    use HasFactory;
+
+    protected static function newFactory()
+    {
+        return new MorphManyInverseCommentModelFactory();
+    }
+
+    protected $table = 'test_comments';
+    protected $fillable = ['id', 'commentable_type', 'commentable_id'];
+
+    public function commentable(): MorphTo
+    {
+        return $this->morphTo('commentable');
+    }
+}
+
+class MorphManyInverseCommentModelFactory extends Factory
+{
+    protected $model = MorphManyInverseCommentModel::class;
+    public function definition()
+    {
+        return [
+            'commentable_type' => MorphManyInversePostModel::class,
+            'commentable_id' => MorphManyInversePostModel::factory(),
+        ];
+    }
+}
